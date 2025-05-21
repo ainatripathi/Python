e5baa41eb02fc926f1e7027682940cd57448daa9
Aina Tripathi <ainatripathi07@gmail.com>
2025-05-21 14:12:08 +0530

    Add files via upload

---
+++ Day 2/data.py
@@ -0,0 +1,28 @@
+name="Aina Tripathi"
+roll_no=44
+grade='A'
+per=94.58
+print("Student name is ",name)
+print(type(name))
+print(id(name))
+print("Student Roll No. is ",roll_no)
+print(type(roll_no))
+print(id(roll_no))
+print("Student grade is ",grade)
+print(type(grade))
+print(id(grade))
+print("Student percentage is ",per)
+print(type(per))
+print(id(per))
+a=True
+b=False
+print(a)
+print(b)
+print(a+a)
+print(a+b)
+x=[10, 30, 255, 100, 15]
+y=bytes(x)
+print(type(y))
+a=[256, 300, 243]
+b=bytearray(a)
+print(type(b))
